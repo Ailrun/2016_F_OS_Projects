8967916a60bbf7b518705b9326805297253ef82b
ailrun <jjc9310@gmail.com>
2016-10-03 23:48:41 +0900

    Add test program file

---
+++ test/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+int main(void)
+{
+	long res = syscall(384);
+	printf("Res: %ld\nerror: %s\n", res, strerror(errno));
+
+	return 0;
+}
