8e87841ec00bc689cf61bb575c58125c92ab7720
Mori <mori@mori-VirtualBox.(none)>
2016-10-05 04:55:10 +0900

    used dynamic allocation. check # of process for limit

--- test/test.c
+++ test/test.c
@@ -6,42 +6,28 @@
 #include <string.h>
 #include <linux/prinfo.h>
 
-#define BUFF_LEN 40
+#define BUFF_LEN 200
 #define nullnumber -1
 int pindex = 0;
+int pmax;
 void print_ptree(struct prinfo *, int);
 int main(void)
 {
-	struct prinfo buff[BUFF_LEN];
+	struct prinfo *buff;
+	buff = (struct prinfo*)malloc(BUFF_LEN*sizeof(struct prinfo));
 	int length = BUFF_LEN;
-	long res = syscall(384, buff, &length);
-
-	/*if (res == 0) {
-		printf("%20s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\n",
-		       "comm", "pid", "state",
-		       "Ppid",
-		       "FCpid",
-		       "NSpid",
-		       "uid");
-
-		for (int i = 0; i < BUFF_LEN; i ++) {
-			printf("%20s\t%10d\t%10ld\t%10d\t%10d\t%10d\t%10d\n",
-			       buff[i].comm, buff[i].pid, buff[i].state,
-			       buff[i].parent_pid,
-			       buff[i].first_child_pid,
-			       buff[i].next_sibling_pid,
-			       buff[i].uid);
-		}
-	}
-    */
+	pmax = syscall(384, buff, &length);
+	printf("length : %d\n",pmax);
+	if(pmax == -EINVAL || pmax == -EFAULT) return 1; 
+	
 	print_ptree(buff, 0);
-	printf("Res: %ld\nerror: %s\n", res, strerror(errno));
+
 	return 0;
 }
 
 void print_ptree(struct prinfo *buff, int step)
 {
-	if(pindex >= BUFF_LEN) return;
+	if(pindex >= pmax || pindex >= BUFF_LEN) return;
 	int i;
 	struct prinfo p = buff[pindex];
     for(i=0;i<step;++i) { printf("\t"); }
