21437f993b3e67c55bc4056bb8a91e919d8ce7fa
ailrun <jjc9310@gmail.com>
2016-10-04 19:35:02 +0900

    Add FCpid and NSpid (but FC is weird)

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -1,11 +1,14 @@
 #include <linux/unistd.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
+#include <linux/list.h>
 #include <linux/string.h>
 
 #include <linux/prinfo.h>
 
 static int walk_process_tree(struct prinfo *buf, int *nr);
+static int copy_to_prinfo_from_task(struct prinfo *pr, struct task_struct *task);
+
 asmlinkage int sys_ptree(struct prinfo *buf, int *nr)
 {
 	int success_entries;
@@ -15,11 +18,11 @@ asmlinkage int sys_ptree(struct prinfo *buf, int *nr)
 		errorint = -EINVAL;
 		goto error;
 	}
-	
+
 	/* struct prinfo p; */
 	/* printk("%s,%d,%ld,%d,%d,%d,%d\n", p.comm, p.pid, p.state, */
 	/*	 p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid); */
-	printk("Hello World\n");
+	printk(KERN_EMERG "[OS_SNU_16] Hello World\n");
 
 	success_entries = walk_process_tree(buf, nr);
 
@@ -34,7 +37,8 @@ error:
   and copy process info into 'buf'
   return the total number of entries on success(?)
 */
-static int walk_process_tree(struct prinfo *buf, int *nr) {
+static int walk_process_tree(struct prinfo *buf, int *nr)
+{
 	struct task_struct *task = NULL;
 	int i = 0;
 
@@ -42,20 +46,31 @@ static int walk_process_tree(struct prinfo *buf, int *nr) {
 
 	for_each_process(task) {
 		if (i >= *nr) {
-			printk(KERN_EMERG "[OS_SNU] task list exceeds buffer\n");
+			printk(KERN_EMERG "[OS_SNU_16] task list exceeds buffer\n");
 			break;
 		}
 		if (task == NULL) {
-			printk(KERN_EMERG "[OS_SNU] task is NULL\n");
+			printk(KERN_EMERG "[OS_SNU_16] task is NULL\n");
 			break;
 		}
-		printk(KERN_EMERG "[OS_SNU] %d\n", task->pid);
-		strncpy(buf[i].comm, task->comm, PRINFO_COMM_LENGTH);
-		buf[i].pid = task->pid;
+		printk(KERN_EMERG "[OS_SNU_16] %d\n", task->pid);
+		copy_to_prinfo_from_task(buf+i, task);
 		i++;
 	}
 
 	read_unlock(&tasklist_lock);
 
-	return 0;	
+	return 0;
+}
+
+static int copy_to_prinfo_from_task(struct prinfo *pr, struct task_struct *task)
+{
+	pr->state = task->state;
+	pr->pid = task->pid;
+	pr->uid = task->real_cred->uid;
+	pr->parent_pid = task->real_parent->pid;
+	pr->first_child_pid = list_first_entry(&task->children, struct task_struct, sibling)->pid;
+	pr->next_sibling_pid = list_first_entry(&task->sibling, struct task_struct, sibling)->pid;
+	strncpy(pr->comm, task->comm, PRINFO_COMM_LENGTH);
+	return 0;
 }
--- test/test.c
+++ test/test.c
@@ -6,14 +6,32 @@
 #include <string.h>
 #include <linux/prinfo.h>
 
+#define BUFF_LEN 20
+
 int main(void)
 {
-	struct prinfo buff[20];
-	int length = 20;
+	struct prinfo buff[BUFF_LEN];
+	int length = BUFF_LEN;
 	long res = syscall(384, buff, &length);
-	for (int i = 0; i < 20; i ++) {
-		printf("Comm: %s\tPid: %d\n", buff[i].comm, buff[i].pid);
+
+	if (res == 0) {
+		printf("%20s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\n",
+		       "comm", "pid", "state",
+		       "Ppid",
+		       "FCpid",
+		       "NSpid",
+		       "uid");
+
+		for (int i = 0; i < BUFF_LEN; i ++) {
+			printf("%20s\t%10d\t%10ld\t%10d\t%10d\t%10d\t%10d\n",
+			       buff[i].comm, buff[i].pid, buff[i].state,
+			       buff[i].parent_pid,
+			       buff[i].first_child_pid,
+			       buff[i].next_sibling_pid,
+			       buff[i].uid);
+		}
 	}
+
 	printf("Res: %ld\nerror: %s\n", res, strerror(errno));
 	return 0;
 }
