6e617cc5aaa7913981decde28ef54dc5106a4259
qwerwy1019 <qwerwy1019@naver.com>
2016-10-04 11:24:04 +0900

    print empty process info

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -1,8 +1,12 @@
 #include <linux/unistd.h>
 #include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/prinfo.h>
 
 asmlinkage long sys_ptree(void)
 {
+	struct prinfo p;
+	printk("%s,%d,%ld,%d,%d,%d,%d\n", p.comm, p.pid, p.state, p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
 	printk("Hello World\n");
 	return 0;
 }
