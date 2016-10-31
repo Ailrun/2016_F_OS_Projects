371e775db54c71698dd6f4000e8d742de367f7ae
sooram <rkdtnfka@naver.com>
2016-10-31 16:40:57 +0900

    fix test

--- test/selector/selector.c
+++ test/selector/selector.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
 #include <sys/syscall.h>
 
 #include <linux/rotation.h>
--- test/trial/trial.c
+++ test/trial/trial.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/syscall.h>
 
 #include <linux/rotation.h>
