682c9674905cdc043ee7380a5105fca22040a7e8
ailrun <jjc9310@gmail.com>
2016-11-22 15:50:45 +0900

    Try fix

--- kernel/sched/wrr.c
+++ kernel/sched/wrr.c
@@ -2,15 +2,7 @@
 
 #include <linux/syscalls.h>
 
-int sched_wrr_timeslice = WRR_TIMESLICE;
-
 #define wrr_entity_is_task(wrr_se) (!(wrr_se)->my_q)
-
-#define for_each_wrr_rq(wrr_rq, iter, rq) \
-	for ((void) iter, wrr_rq = &rq->wrr; wrr_rq; wrr_rq = NULL)
-
-#define for_each_sched_wrr_entity(wrr_se) \
-	for (; wrr_se; wrr_se = NULL)
 
 static inline struct task_struct *wrr_task_of(struct sched_wrr_entity *wrr_se)
 {
@@ -23,57 +15,42 @@ static inline struct task_struct *wrr_task_of(struct sched_wrr_entity *wrr_se)
 void init_wrr_rq(struct wrr_rq *wrr_rq)
 {
 	INIT_LIST_HEAD(&wrr_rq->queue);
+	wrr_rq->wrr_weight_total = 0;
 	raw_spin_lock_init(&wrr_rq->wrr_runtime_lock);
 }
 
 static void enqueue_task_wrr(struct rq *rq, struct task_struct *p, int flags)
 {
 	struct wrr_rq *wrr_rq = &rq->wrr;
+	struct sched_wrr_entity *wrr_se = &p->wrr;
 	
-	if(!wrr_rq) {
-		return;
-	}
 	raw_spin_lock(&wrr_rq->wrr_runtime_lock);
-	list_add_tail(&(p->wrr.run_list),&wrr_rq->queue);
+
+	list_add_tail(&wrr_se->run_list, &wrr_rq->queue);
+
 	rq->nr_running++;
+	wrr_rq->wrr_nr_running++;
+	wrr_rq->wrr_weight_total += wrr_se->weight;
+	p->on_rq = 1;
+
 	raw_spin_unlock(&wrr_rq->wrr_runtime_lock);
 }
 
 static void dequeue_task_wrr(struct rq *rq, struct task_struct *p, int flags)
 {
 	struct wrr_rq *wrr_rq = &rq->wrr;
-	
-	if(!wrr_rq)
-		return;
-	raw_spin_lock(&wrr_rq->wrr_runtime_lock);
-	list_del_init(&wrr_rq->queue);
-	rq->nr_running--;
-	raw_spin_unlock(&wrr_rq->wrr_runtime_lock);
-}
-
-static void
-requeue_wrr_entity(struct wrr_rq *wrr_rq, struct sched_wrr_entity *wrr_se, int head)
-{
-	if (!list_empty(&wrr_se->run_list)) {
-		struct list_head *queue = &wrr_rq->queue;
-
-		if (head)
-			list_move(&wrr_se->run_list, queue);
-		else
-			list_move_tail(&wrr_se->run_list, queue);
-	}
-}
-
-static void
-requeue_task_wrr(struct rq *rq, struct task_struct *p, int head)
-{
 	struct sched_wrr_entity *wrr_se = &p->wrr;
-	struct wrr_rq *wrr_rq;
+	
+	raw_spin_lock(&wrr_rq->wrr_runtime_lock);
 
-	for_each_sched_wrr_entity(wrr_se) {
-		wrr_rq = wrr_se->wrr_rq;
-		requeue_wrr_entity(wrr_rq, wrr_se, head);
-	}
+	list_del_init(&wrr_se->run_list);
+
+	rq->nr_running--;
+	wrr_rq->wrr_nr_running--;
+	wrr_rq->wrr_weight_total -= wrr_se->weight;
+	p->on_rq = 0;
+
+	raw_spin_unlock(&wrr_rq->wrr_runtime_lock);
 }
 
 static void yield_task_wrr(struct rq *rq)
@@ -83,7 +60,7 @@ static void yield_task_wrr(struct rq *rq)
 static bool
 yield_to_task_wrr(struct rq *rq, struct task_struct *p, bool preempt)
 {
-	return false;
+	return true;
 }
 
 static void
@@ -91,50 +68,21 @@ static void
 {
 }
 
-static struct sched_wrr_entity *pick_next_wrr_entity(struct rq *rq,
-								struct wrr_rq *wrr_rq)
+static struct task_struct *pick_next_task_wrr(struct rq *rq)
 {
-	struct sched_wrr_entity *next = NULL;
-	struct list_head *queue = &wrr_rq->queue;
-
-	next = list_entry(queue->next, struct sched_wrr_entity, run_list);
-
-	return next;
-}
-
-static struct task_struct *_pick_next_task_wrr(struct rq *rq)
-{
-	struct sched_wrr_entity *wrr_se;
-	struct task_struct *p;
-	struct wrr_rq *wrr_rq;
-	
-	wrr_rq = &rq->wrr;
+	struct wrr_rq *wrr_rq = &rq->wrr;
+	struct sched_wrr_entity *next_se;
+	struct task_struct *next;
 
 	if (!wrr_rq->wrr_nr_running)
 		return NULL;
 
-	do {
-		wrr_se = pick_next_wrr_entity(rq, wrr_rq);
-		BUG_ON(!wrr_se);
-		wrr_rq = wrr_se->my_q;
-	} while (wrr_rq);
+	next_se = list_entry(&wrr_rq->queue, struct sched_wrr_entity, run_list);
+	next = wrr_task_of(next_se);
 
-	p = wrr_task_of(wrr_se);
-	p->se.exec_start = rq->clock_task;
+	next->wrr.time_slice = next->wrr.time_slice * WRR_TIMESLICE;
 
-	return p;
-}
-
-static struct task_struct *pick_next_task_wrr(struct rq *rq)
-{
-	struct task_struct *p = _pick_next_task_wrr(rq);
-
-#ifdef CONFIG_SMP
-
-//	rq->post_schedule = 0;
-#endif
-
-	return p;
+	return next;
 }
 
 static void put_prev_task_wrr (struct rq *rq, struct task_struct *p)
@@ -189,35 +137,42 @@ static void set_curr_task_wrr(struct rq *rq)
 
 static void task_tick_wrr(struct rq *rq, struct task_struct *p, int queued)
 {
+	struct wrr_rq *wrr_rq = &rq->wrr;
 	struct sched_wrr_entity *wrr_se = &p->wrr;
 	
 	if (p->policy != SCHED_WRR)
 		return;
 
+	raw_spin_lock(&wrr_rq->wrr_runtime_lock);
+
 	if (--wrr_se->time_slice)
-		return;
+		goto skip;
 
-	wrr_se->time_slice = sched_wrr_timeslice;
+	if (wrr_se->run_list.prev != wrr_se->run_list.next) {
+		list_move_tail(&wrr_se->run_list, &wrr_rq->queue);
 
-	for_each_sched_wrr_entity(wrr_se) {
-		if (wrr_se->run_list.prev != wrr_se->run_list.next) {
-			requeue_task_wrr(rq,p,0);
-			set_tsk_need_resched(p);
-			return;
-		}
-	}
+		set_tsk_need_resched(p);
+	} else
+		wrr_se->time_slice = wrr_se->weight * WRR_TIMESLICE;
+
+skip:
+	raw_spin_unlock(&wrr_rq->wrr_runtime_lock);
 }
 
 static void task_fork_wrr(struct task_struct *p)
 {
+	p->wrr.weight = p->real_parent->wrr.weight;
+	p->wrr.time_slice = p->wrr.weight * WRR_TIMESLICE;
 }
 
-static void switched_from_wrr(struct rq *this_rq, struct task_struct *task)
+static void switched_from_wrr(struct rq *this_rq, struct task_struct *p)
 {
 }
 
-static void switched_to_wrr(struct rq *this_rq, struct task_struct *task)
+static void switched_to_wrr(struct rq *this_rq, struct task_struct *p)
 {
+	p->wrr.weight = 10;
+	p->wrr.time_slice = p->wrr.weight * WRR_TIMESLICE;
 }
 
 static void
@@ -227,7 +182,7 @@ prio_changed_wrr(struct rq *this_rq, struct task_struct *task, int oldprio)
 
 static unsigned int get_rr_interval_wrr(struct rq *rq, struct task_struct *task)
 {
-	return 0;
+	return WRR_TIMESLICE;
 }
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
@@ -285,12 +240,12 @@ extern void print_wrr_rq(struct seq_file *m, int cpu, struct wrr_rq *wrr_rq);
 
 void print_wrr_stats(struct seq_file *m, int cpu)
 {
-	struct wrr_rq *iter;
 	struct wrr_rq *wrr_rq;
 
 	rcu_read_lock();
-	for_each_wrr_rq(wrr_rq, iter, cpu_rq(cpu))
-		print_wrr_rq(m, cpu, wrr_rq);
+	
+	wrr_rq = &cpu_rq(cpu)->wrr;
+	print_wrr_rq(m, cpu, wrr_rq);
 	rcu_read_unlock();
 }
 #endif /* CONFIG_SCHED_DEBUG */
