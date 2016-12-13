8e1459a6168caa04b423f8809ed76fa5bfc4699e
ailrun <jjc9310@gmail.com>
2016-12-13 19:45:12 +0900

    Fix tb.sh for efficient error checking

--- tb.sh
+++ tb.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
-T_DIR="test"
-T_FILE="$T_DIR/test"
-
 if [ "$#" -eq 1 ]; then
     case $1 in
         0)
-            ./build.sh tizen_tm1 USR ;;
+            ./build.sh tizen_tm1 USR 2> error.log
+	    echo "Compile Error Lists:"
+	    cat error.log | grep "error:";;
         1)
+	    sudo ls
             scripts/mkmodimg.sh
             tar cf IMAGE.tar -C arch/arm/boot dzImage -C ../../../usr/tmp-mod modules.img ;;
         2)
             sudo lthor IMAGE.tar ;;
-        3)
-            make -C $T_DIR
-            sdb push $T_FILE /home/developer/$T_FILE
-            sdb shell "cd ~; ./$T_FILE"
     esac
 else
     echo "Usage: ./tb <build stage>"
