233f3d2a86727c86576951ca8907bb5b5ce348ab
ailrun <jjc9310@gmail.com>
2016-12-13 22:32:17 +0900

    Add set_gps_location impl

--- kernel/gps.c
+++ kernel/gps.c
@@ -1,8 +1,24 @@
 #include <linux/gps.h>
 #include <linux/syscalls.h>
+#include <linux/errno.h>
+
+static DEFINE_RWLOCK(dev_loc_lock);
+static DEFINE_GPS_LOCATION(dev_loc);
 
 SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc)
 {
+        struct gps_location tmp_loc;
+
+	if (loc == NULL)
+		return -EINVAL;
+
+	if (copy_from_user(&tmp_loc, loc, sizeof(tmp_loc)))
+		return -EFAULT;
+
+	write_lock(&dev_loc_lock);
+	memcpy(&dev_loc, &tmp_loc, sizeof(tmp_loc));
+	write_unlock(&dev_loc_lock);
+
 	return 0;
 }
 
