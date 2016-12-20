c2384e8b2fd6c85c8d0da0009c5ab7fdb7e34d99
s-01 <siso1591@daum.net>
2016-12-20 18:51:03 +0900

    Update get gps

--- kernel/gps.c
+++ kernel/gps.c
@@ -1,6 +1,7 @@
 #include <linux/gps.h>
 #include <linux/syscalls.h>
 #include <linux/errno.h>
+#include <linux/namei.h>
 
 static DEFINE_RWLOCK(dev_loc_lock);
 static DEFINE_GPS_LOCATION(dev_loc);
@@ -27,7 +28,31 @@ SYSCALL_DEFINE1(set_gps_location, struct gps_location __user *, loc)
 SYSCALL_DEFINE2(get_gps_location, const char __user *, pathname,
 		struct gps_location __user *, loc)
 {
-	return 0;
+	struct path path;
+	struct inode *inode;
+	struct gps_location nloc;
+	int errchk;
+
+	if (pathname == NULL || loc == NULL)
+		return -EINVAL;
+
+	if(user_path(pathname, &path))
+		return -EINVAL;
+
+	inode = path.dentry->d_inode;
+
+	if(!inode->i_op->get_gps_location || inode->i_mode)
+		return -ENODEV;
+
+	errchk = inode->i_op->get_gps_location(inode, &nloc);
+	path_put(&path);
+
+	if(copy_to_user(loc, &nloc, sizeof(struct gps_location)))
+		return -EFAULT;
+	
+	if(errchk)
+		return errchk;
+	else return -1;
 }
 
 void get_device_location(struct gps_location *loc)
